4e30ab66b86eb9934c74feb9d03eaba18f08b5aa
Nathan Bell <nathan.bell421@icloud.com>
2021-01-07 22:46:00 -0600

    add ninja

---
+++ script.js
@@ -0,0 +1,22 @@
+class Ninja {
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.speed = 3;
+        this.strength = 3;
+    }
+    sayName(){
+        console.log(`My name is ${this.name}`);
+    }
+    showStats(){
+        console.log(this);
+    }
+    drinkSake(){
+        this.health += 10;
+    }
+}
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
+ninja1.showStats();
